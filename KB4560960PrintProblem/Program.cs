e17d2e196f12e2ab88299645b9bc5fcc381497e7
Toshko <rav3nlabs@gmail.com>
2020-06-26 12:55:03 +0300

    Add project files.

---
+++ KB4560960PrintProblem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Printing;
+
+namespace KB4560960PrintProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var printDocument = new PrintDocument())
+            {
+                printDocument.PrinterSettings = new PrinterSettings { PrinterName = "Microsoft Print to PDF" };
+                printDocument.PrintPage += PrintDocument_PrintPage;
+                printDocument.Print();
+            }
+        }
+
+        private static void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.HasMorePages = false;
+            using (var bmp = Image.FromFile(@"broken.png"))
+            {
+                e.Graphics.DrawImage(bmp, new PointF());
+            }
+        }
+    }
+}
